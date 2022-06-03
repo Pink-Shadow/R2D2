95dc07ba96839bc76cdf0e1aafb5928747007828
Jasper <jaspervdpost@gmail.com>
2022-06-03 16:19:02 +0200

    reworked Nearest neighbour algorithm. finished it."
    Co-authored-by: StephanZaaijer stephan.zaaijer@student.hu.nl

--- NNA.py
+++ NNA.py
@@ -1,6 +1,7 @@
 import copy
 import math
-import parser
+from re import I
+from gcode_parser import parse_gcode, getStartToEnd
 
 """! @file
      @brief Python code to turn g-code into xy coordinates so the printer understands it.
@@ -8,12 +9,13 @@ import parser
 
 
 def toFile(data):
-    file = open("new_gcode.txt", 'w')
-    for line in data:
-        file.write(str(line))
-        file.write("\n")
-    file.close()
-
+    with open("new_gcode.txt", 'w') as file:
+        for i, line in enumerate(data):
+            if i == 0:
+                file.write(f"G0 X{line[0].X} Y{line[0].Y}\n")
+            if not (line[0].X == data[i-1][0].X and line[0].Y == data[i-1][0].Y):
+                file.write(f"G0 X{line[0].X} Y{line[0].Y}\n")
+            file.write(f"G1 X{line[1].X} Y{line[1].Y}\n")
 
 def calculateDistance(x1, y1, x2, y2):
     dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -21,89 +23,29 @@ def calculateDistance(x1, y1, x2, y2):
 
 
 def optimize(lines):
-    linesToPop = copy.deepcopy(lines)
+    new_lines = []
+    current_loc = [0, 0]
 
-    newGcode = []
-    index = 0
-    tmpclass = parser.gcode_class()
-    shortestLine = parser.gcode_class()
-    len_lines = len(lines) -1
+    l = len(lines)
+    while l > 0:
+        shortest_line = None
+        shortest_dist = float('inf')
+        for i in range(l):
+            dist = calculateDistance(current_loc[0], current_loc[1], lines[i][0].X, lines[i][0].Y)
+            if dist < shortest_dist:
+                shortest_dist = dist
+                shortest_line = i
 
-    while len_lines:
-        added = False
-        distanceNotNull = False
-        lastDistance = 30000
-        print(len_lines)
-        currentXbegin = lines[len_lines][0].X
-        currentXend = lines[len_lines][1].X
-        currentYbegin = lines[len_lines][0].Y
-        currentYend = lines[len_lines][1].Y
-
-        for i in range(len(linesToPop)):
-            added = False
-            if (currentXend == lines[i][1].X & currentYend == lines[i][1].Y) & (
-                    currentXbegin == lines[i][0].X & currentYbegin == lines[i][0].Y):
-                # gelijk aan zichzelf (eindpunten
-                # print("if ")
-                # shortestIndex = i
-
-                continue
-
-            elif (currentXend == lines[i][0].X):
-                if (currentYend == lines[i][0].Y):
-                    # aan elkaar vast
-                    tmpclass.G = 1
-                    tmpclass.X = currentXend
-                    tmpclass.Y = currentYend
-                    newGcode.append(copy.copy(tmpclass))
-                    print("g1 eindpunt current", tmpclass)
-                    print(f"g1 beginpunt nieuw G1 X{lines[i][0].X} Y{lines[i][0].Y}")
-
-                    tmpclass.G = 1
-                    tmpclass.X = lines[i][1].X
-                    tmpclass.Y = lines[i][1].Y
-                    newGcode.append(copy.copy(tmpclass))
-                    print("g1 eindpunt new", tmpclass)
-
-                    index = i
-
-                    added = True
-                    break
-
-            else:
-
-                distance = calculateDistance(currentXend, currentYend, lines[i][0].X, lines[i][0].Y)
-                # print(f"distance {distance}, last {lastDistance}")
-                if distance < lastDistance:
-                    lastDistance = distance
-                    shortestLine = lines[i][0]
-                    index = i
-                    added = False
-        # print(distanceNotNull)
-        if not added:
-            # print(distanceNotNull)
-            # print("else")
-            tmpclass.G = 0
-            tmpclass.X = shortestLine.X
-            tmpclass.Y = shortestLine.Y
-            newGcode.append(copy.copy(tmpclass))
-            newGcode.append(copy.copy(shortestLine))
-            print("g0 korte", shortestLine)
-        linesToPop[index] = 0
-
-        len_lines -= 1
-    return newGcode
+        new_lines.append(lines[shortest_line])
+        print(shortest_line, len(lines))
+        current_loc = [lines[shortest_line][1].X, lines[shortest_line][1].Y]
+        lines.pop(shortest_line)
+        l -= 1
+    
+    return new_lines
 
 
-coords = parser.parse_gcode()
-
-lijnen = parser.getStartToEnd(coords)
-# for x in lijnen:
-#     print(x)
-
+coords = parse_gcode()
+lijnen = getStartToEnd(coords)
 optimized = optimize(lijnen)
-#
-# for x in optimized:
-#     print(x)
-# print(lines)
 toFile(optimized)
