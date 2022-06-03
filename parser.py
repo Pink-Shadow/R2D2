2225e3a5430d4b271acdfc315db153b4aedfbc53
Jasper <jaspervdpost@gmail.com>
2022-06-03 13:44:01 +0200

    TSP algorithm and gcode writer implemented

--- parser.py
+++ parser.py
@@ -1,54 +1,97 @@
 import copy
+import math
+import itertools as it
 
-class gcode_class:
-    def __init__(self):
-        self.G: int = 0
-        self.X: int = 0
-        self.Y: int = 0
+class DrawObject:
+    def __init__(self, id):
+        self.begin = []
+        self.end = []
+
+        self.id = id
+        self.Gcode = []
 
     def __repr__(self):
-        return str(self)
+        return f"{self.id}"
+        
+        
+    def get_gcode(self):
+        gcode = []
 
-    def __str__(self):
-        return f"G{str(self.G)} X{str(self.X)} Y{str(self.Y)}"
+        gcode.append(f"G0 X{self.begin[0]} Y{self.begin[1]}")
+        gcode.append(f"G0 {self.Gcode[0][self.Gcode[0].index('X'):]}")
+        for line in self.Gcode:
+            gcode.append(line)
 
-
-def getStartToEnd(coords):
-    tmp = []
-    listWithLines = []
-    for x in range(len(coords) - 1):
-        if coords[x + 1].G != 0:
-            tmp.append(coords[x])
-            tmp.append(coords[x + 1])
-            listWithLines.append(copy.copy(tmp))
-            tmp = []
-    return listWithLines
-
+        return gcode
+        
 
 def parse_gcode():
-    coords = []
+    objects = []
+    id = 1
     with open(f"gcode.txt", "r") as gcode:
         data = gcode.readlines()
-        for line in data:
-            # if not line.startswith("G0"):
-            tmp = gcode_class()
-            elements = line.split(' ')
-            for e in elements:
-                c = e[0]
-                if c == 'X':
-                    tmp.X = int(e.strip()[1:])
-                if c == 'Y':
-                    tmp.Y = int(e.strip()[1:])
-                if c == 'G' and len(elements) > 1:
-                    tmp.G = int(e.strip()[1:])
-            coords.append(copy.copy(tmp))
-    return coords
+        tmp = DrawObject(id)
+        for i, line in enumerate(data):
+            line = line.strip()
+            if line.startswith('G0'):
+                tmp.Gcode.append(line)
+
+                elements = line.split(' ')
+                for e in elements:
+                    c = e[0]
+                    if c == 'X':
+                        tmp.begin.append(int(e.strip()[1:]))
+                    if c == 'Y':
+                        tmp.begin.append(int(e.strip()[1:]))
+
+            elif line.startswith('G1') and data[i+1].startswith('G0'):
+                tmp.Gcode.append(line)
+
+                elements = line.split(' ')
+                for e in elements:
+                    c = e[0]
+                    if c == 'X':
+                        tmp.end.append(int(e.strip()[1:]))
+                    if c == 'Y':
+                        tmp.end.append(int(e.strip()[1:]))
+                objects.append(copy.deepcopy(tmp))
+
+                id +=1 
+                tmp = DrawObject(id)
+                
+            else:
+                tmp.Gcode.append(line)
+
+    return objects
 
 
+def write_route_gcode_to_file(route):
+    with open(f"route.txt", "w") as gcode:
+        for object in route:
+            code = object.get_gcode()
+            for line in code:
+                gcode.write(line + "\n")
+
+
+##################################################
+
+def calculateDistance(x1,y1,x2,y2):
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return dist
 
 if __name__ == "__main__":
-    coords = parse_gcode()
-    coords = getStartToEnd(coords)
+    objects = parse_gcode()
+    routes = list(it.permutations(objects)) 
+    
+    shortest_route = None
+    shortest_dist = -1
+    for route in routes:    # [1, 2, 0, 7, 8]
+        size = 0
+        for i in range(len(route)-1):
+            size += calculateDistance(route[i].end[0], route[i].end[1], route[i+1].begin[0], route[i+1].begin[1])
+        if shortest_dist == -1 or size < shortest_dist:
+            shortest_route = route
+            shortest_dist = size
 
-    for x in coords:
-        print(x)
+    print(f"Shortest route: {shortest_route}\nShortest distance: {shortest_dist}")
+    write_route_gcode_to_file(shortest_route)
