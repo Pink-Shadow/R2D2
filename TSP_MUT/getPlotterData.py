a9799c4b7aa7d7b963c0bf2946fad1ded4a20992
StephanZaaijer <stephan.zaaijer@gmail.com>
2022-06-19 11:49:17 +0200

    executed output of tsp and tspmut with CalcaluteSteps and updated CalculateSteps to store bmp-files to visualize the output

---
+++ TSP_MUT/getPlotterData.py
@@ -0,0 +1,24 @@
+
+#for files in the folder
+import os
+import subprocess
+
+#loop through sub-directory in order to get all files
+
+
+# loop through the files in the folder
+print(os.getcwd())
+
+#open txtfile
+outputfile = open("output_TSPMUT.txt", "w")
+for directory  in os.listdir(os.getcwd()):
+#check if file is directory
+    if os.path.isdir(directory):
+        for filename in os.listdir(directory):
+            if filename.endswith(".txt"):
+                bmpfilename = filename.replace(".txt", ".bmp")
+                tmp = subprocess.run(["D:\\Users\\Stephan\\Documents\\GitHub\\R2D2KLASB\\Research\\CalculateSteps.exe", directory + "\\" + filename, directory+ '\\' + bmpfilename], stdout=subprocess.PIPE)
+                print(tmp.stdout.decode('utf-8'))
+                outputfile.write(directory + "\\" + filename + tmp.stdout.decode('utf-8') + "\n")
+
+outputfile.close()
